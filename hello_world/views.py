5f7a2c74c9ac51b9a66650ebfbf529c9bbbc8356
Amir Mohammadi <amirmhmdi95@gmail.com>
2026-08-25 20:06:18 +0100

    req type in index method

--- hello_world/views.py
+++ hello_world/views.py
@@ -4,4 +4,8 @@ from django.http import HttpResponse
 # Create your views here.
 
 def index(request):
-    return HttpResponse("Hello, world. You're at the hello_world index.")
+    if request.method == "POST":
+        return HttpResponse("You must have POSTed something")
+    else:
+        return HttpResponse(request.method)
+    # return HttpResponse("Hello, world. You're at the hello_world index.")
